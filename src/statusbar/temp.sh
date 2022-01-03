b75536e5bf24c8eb6007c159a1bf2b07b441fc0b
Dušan Stanković <todooc4@gmail.com>
2022-01-03 12:30:23 +0100

    shamelessly forked scripts from 7aske <3

--- src/statusbar/temp.sh
+++ src/statusbar/temp.sh
@@ -6,8 +6,19 @@ case $BLOCK_BUTTON in
 	1) notify-send "CPU hogs" "$(ps axch -o cmd:15,%cpu --sort=-%cpu | head)" ;;
 esac
 
-temp="$(sensors | awk '/Package id 0:/{print substr($4, 2)} /Tdie|Tctl/{print substr($2, 2)}')"
+temp="$(sensors 2>/dev/null | awk '/Package id 0:/{print substr($4, 2)} /Tdie|Tctl/{print substr($2, 2)}' | head -n 1)"
 temp_val="$(echo $temp | awk '{print substr($0, 1, length($0)-4)}')"
+gpu_temp="$(sensors 2>/dev/null | awk '/edge:/ {print $2}')"
+gpu_temp_val="$(echo $gpu_temp | awk '{print substr($0, 1, length($0)-4)}')"
+
+gpu_color="$color7"
+if [ "$gpu_temp_val" -ge 85 ]; then
+	gpu_color="$theme11"
+elif [ "$gpu_temp_val" -ge 70 ]; then
+	gpu_color="$theme12"
+elif [ "$gpu_temp_val" -ge 55 ]; then
+	gpu_color="$theme13"
+fi
 
 color="$color7"
 if [ "$temp_val" -ge 60 ]; then
@@ -18,4 +29,4 @@ elif [ "$temp_val" -ge 40 ]; then
 	color="$theme13"
 fi
 
-printf "<span color=\"%s\">%s</span>\n" $color $temp
+printf "CPU: <span color=\"%s\">%s</span> GPU: <span color=\"%s\">%s</span>\n" $color $temp $gpu_color $gpu_temp
